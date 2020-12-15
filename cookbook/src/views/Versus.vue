0b4816c7eb42d07d7d9b4c0bcaa32348dabd1879
William Andersson <wandersson.98@gmail.com>
2020-12-15 18:16:34 +0100

    functions working with front end, todo create dynamic prod/dev environment urls

--- cookbook/src/views/Versus.vue
+++ cookbook/src/views/Versus.vue
@@ -3,9 +3,9 @@
     <v-card-title>View matchup specific techniques</v-card-title>
     <v-container>
       <v-row>
-        <v-col v-for="char in characters" v-bind:key="char.imgSrc">
+        <v-col v-for="char in characters" v-bind:key="char.id">
           <v-btn v-on:click="onCharacterButtonClick(char)">
-            {{ char.name }}
+            {{ char.data.name }}
           </v-btn>
         </v-col>
       </v-row>
@@ -14,54 +14,30 @@
 </template>
 
 <script>
+const axios = require("axios");
 export default {
   data: () => ({
-    characters: [
-      {
-        name: "Falco",
-        imgSrc:
-          "https://static.wikia.nocookie.net/ssb/images/e/e1/Falco_Head_01_%28SSBM%29.png/revision/latest/scale-to-width-down/24?cb=20160118221914",
-      },
-      {
-        name: "Falco",
-        imgSrc:
-          "https://static.wikia.nocookie.net/ssb/images/e/e1/Falco_Head_01_%28SSBM%29.png/revision/latest/scale-to-width-down/24?cb=20160118221914",
-      },
-      {
-        name: "Falco",
-        imgSrc:
-          "https://static.wikia.nocookie.net/ssb/images/e/e1/Falco_Head_01_%28SSBM%29.png/revision/latest/scale-to-width-down/24?cb=20160118221914",
-      },
-      {
-        name: "Falco",
-        imgSrc:
-          "https://static.wikia.nocookie.net/ssb/images/e/e1/Falco_Head_01_%28SSBM%29.png/revision/latest/scale-to-width-down/24?cb=20160118221914",
-      },
-      {
-        name: "Falco",
-        imgSrc:
-          "https://static.wikia.nocookie.net/ssb/images/e/e1/Falco_Head_01_%28SSBM%29.png/revision/latest/scale-to-width-down/24?cb=20160118221914",
-      },
-      {
-        name: "Falco",
-        imgSrc:
-          "https://static.wikia.nocookie.net/ssb/images/e/e1/Falco_Head_01_%28SSBM%29.png/revision/latest/scale-to-width-down/24?cb=20160118221914",
-      },
-      {
-        name: "Falco",
-        imgSrc:
-          "https://static.wikia.nocookie.net/ssb/images/e/e1/Falco_Head_01_%28SSBM%29.png/revision/latest/scale-to-width-down/24?cb=20160118221914",
-      },
-    ],
+    characters: [],
   }),
 
   methods: {
     onCharacterButtonClick(character) {
       this.$router.push({
         name: "VersusChar",
-        params: { character: character.name },
+        params: { character: character.data.name },
       });
     },
+  },
+
+  created() {
+    axios({
+      method: "GET",
+      url:
+        "http://localhost:5001/thecookbook-5c258/us-central1/app/api/characters",
+    }).then((res) => {
+      this.characters = res.data;
+      console.log(this.characters);
+    });
   },
 };
 </script>
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+admin.initializeApp();
+const db = admin.firestore();
+
+app.use(cors("*"));
+
+app.get("/api/characters", async (req, res) => {
+  const charsRef = db.collection("characters");
+  const characters = await charsRef.get();
+
+  if (characters.empty) {
+    res.sendStatus(204);
+  } else {
+    let out = [];
+    characters.forEach((doc) => {
+      out.push({ id: doc.id, data: doc.data() });
+    });
+    res.status(200).json(out);
+  }
+});
+
+exports.app = functions.https.onRequest(app);
